32fd174f7a93dcf180bbaf1002d03ebefb1218d9
Tim Gubner <tim.gubner@cwi.nl>
2020-02-20 23:02:14 +0100

    Initializing areas in example2

--- example2.cpp
+++ example2.cpp
@@ -1,11 +1,22 @@
 #include "progress_meter.hpp"
 #include <vector>
+#include <string.h>
 
 static std::vector<char*> allocations;
 
 void task(bool alloc) {
 	int size = rand() % 1024;
-	allocations.push_back(new char[size]);
+	if (size < 0) {
+		size = -size;
+	}
+
+	if (!size) {
+		return;
+	}
+
+	char* p = new char[size];
+	allocations.push_back(p);
+	memset(p, 0, size);
 }
 
 int main() {
